59ce08b830a69588a9bbc0c204b313725880d81c
DEV_T <tewsk132@gmail.com>
2024-08-31 14:52:08 +0700

    add set default name and binding

--- projects/new-micro-front-end/features/category-add/category-add.component.ts
+++ projects/new-micro-front-end/features/category-add/category-add.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CategoryRequest } from '../../models/category-request';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-category-add',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './category-add.component.html',
   styleUrl: './category-add.component.scss'
 })
 export class CategoryAddComponent {
+  public category: CategoryRequest = new CategoryRequest();
+
+  constructor(){
+    this.category.name = 'Category Name';
+  }
+
 
 }
