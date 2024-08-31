9f9ccdc5913f378cb00a912c0cfe64bfe0dabc18
DEV_T <tewsk132@gmail.com>
2024-08-31 16:01:01 +0700

    add call miro front end

--- projects/boot-camp/src/app/app.routes.ts
+++ projects/boot-camp/src/app/app.routes.ts
@@ -5,5 +5,12 @@ export const routes: Routes = [
         path: 'categories',
         loadComponent : () => import('./features/categories/categories.component').then(m=> m.CategoriesComponent)
         //component : CategoriesComponent
+    },
+    {
+        path :'categories/add',
+        loadComponent: () => 
+            import('./features/category-add/category-add.component').then(
+                m => m.CategoryAddComponent,
+        ),
     }
 ];
---
+++ projects/boot-camp/src/app/features/category-add/category-add.component.ts
@@ -0,0 +1,46 @@
+// import { Component, ViewContainerRef } from '@angular/core';
+// import { loadRemoteModule } from '@angular-architects/module-federation';
+
+// @Component({
+//   selector: 'app-category-add',
+//   standalone: true,
+//   imports: [],
+//   template: '',
+// })
+// export class CategoryAddComponent {
+//   constructor(private viewContainer: ViewContainerRef) {}
+
+//   async ngOnInit() {
+//     const module = await loadRemoteModule({
+//       type: 'module',
+//       remoteEntry: 'http://localhost:4201/remoteEntry.js',
+//       exposedModule: './CategoryAdd',
+//     });
+
+//     // console.log('module', module);
+//     // this.viewContainer.createComponent(module.CategoryAddComponent);
+//   }
+// }
+import { Component, ViewContainerRef } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
+@Component({
+  selector: 'app-category-add',
+  standalone: true,
+  imports: [],
+  template: '',
+})
+export class CategoryAddComponent {
+  constructor(private viewContainer: ViewContainerRef) {}
+
+  async ngOnInit() {
+    const module = await loadRemoteModule({
+      type: 'module',
+      remoteEntry: 'http://localhost:4201/remoteEntry.js',
+      exposedModule: './CategoryAdd',
+    });
+
+    console.log('module', module);
+    this.viewContainer.createComponent(module.CategoryAddComponent)
+  }
+}
