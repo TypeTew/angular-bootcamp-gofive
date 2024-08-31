bf6c1be7887503174c1e90fb53e93ab64cdea82d
DEV_T <tewsk132@gmail.com>
2024-08-31 13:09:53 +0700

    add first commit

---
+++ projects/boot-camp/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CategoriesComponent } from './features/categories/categories.component';
+
+export const routes: Routes = [
+    {
+        path: 'categories',
+        component : CategoriesComponent
+    }
+];
